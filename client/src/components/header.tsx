326778ebba013f8e62b1adc8def8c288aaceed05
Aditya raj <aditya758.hitcsecs2020@gmail.com>
2024-05-17 10:48:00 +0530

    added mixin,scass and header page

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,20 +1,77 @@
 import { Link } from "react-router-dom";
-import { FaSearch, FaShoppingBag } from "react-icons/fa";
+import { FaSearch, FaShoppingBag, FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
+import { useState } from "react";
 
 
 const user = {
-    _id:"34"
+    _id:"34",
+    role:"admin"
 }
 
+
 const Header = () => {
-  return <nav>
-    <Link to={"/"}>Home</Link>
-    <Link to={"/search"}>
+
+  const logoutHandler = () => {
+    setIsOpen(false);
+  }
+
+ const [isOpen,setIsOpen] = useState<boolean>(false);
+  
+
+  return <nav className="header">
+
+    <Link
+      onClick={() => setIsOpen(false)}
+      to={"/"}
+      >
+        Header
+     </Link>
+
+    <Link
+     to={"/search"}
+     onClick={() => setIsOpen(false)}
+     >
        <FaSearch/>
     </Link>
-    <Link to={"/cart"}>
+
+    <Link
+     to={"/cart"}
+     onClick={() => setIsOpen(false)}
+     >
         <FaShoppingBag/>
     </Link>
+
+    {
+      user?._id ? (
+      <>
+        <button onClick={()=>setIsOpen((prev) => !prev)}>
+          <FaUser/>
+        </button>
+        <dialog open={isOpen} >
+          <div>
+            {
+              user.role === "admin" && (
+                <Link to="/admin/dashboard">Admin</Link>
+              )
+            }
+            <Link
+             to="/orders"
+             onClick={()=>setIsOpen(false)}
+             >
+               Orders
+            </Link>
+            <button onClick={logoutHandler}>
+              <FaSignOutAlt/>
+            </button>
+          </div>
+          
+        </dialog>
+      </>
+      ) : ( <Link to={"/login"} onClick={()=>setIsOpen(false)}>
+          <FaSignInAlt/>
+      </Link>
+    )}
+
  </nav>
 }
 
